4b4d325ba535de27d62ee2ee14f226d7716dfda9
Sd37289 <sd37289@gmail.com>
2023-10-20 14:41:09 +0300

    pakete örnek metodlar eklendi

--- package_exemple/lib/package_exemple.dart
+++ package_exemple/lib/package_exemple.dart
@@ -1,7 +1,35 @@
 library package_exemple;
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+
+
+class UyumExemple {
+  double UyumFunc_1 (){
+    return 10;
+  } 
+
+  static UyumFunc_2 (){
+    return 'Uyumsoft';
+  }
+}
+
+class UyumContainer extends StatelessWidget {
+  const UyumContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 25,
+      width: 25,
+      color: Colors.red,      
+    );
+  }
 }
